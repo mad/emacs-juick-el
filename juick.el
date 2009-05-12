3cf83181c7b5f7af1772e4e0b92761f7152c379d
mad <owner.mad.epa@gmail.com>
2009-05-12 16:58:57 +0400

    add *bold*, /italic/, _underline_ faces

--- juick.el
+++ juick.el
@@ -49,7 +49,31 @@
   "face for displaying tags"
   :group 'juick-faces)
 
+(defface juick-bold-face
+  '((t (:weight bold :slant normal)))
+  "face for displaying bold text"
+  :group 'juick-faces)
+
+(defface juick-italic-face
+  '((t (:slant italic)))
+  "face for displaying italic text"
+  :group 'juick-faces)
+
+(defface juick-underline-face
+  '((t (:underline t :slant normal)))
+  "face for displaying underline text"
+  :group 'juick-faces)
+
+
 (defvar juick-overlays nil)
+
+;; from http://juick.com/help/
+(defvar juick-id-regex "\\(#[0-9]+\\(/[0-9]+\\)?\\)")
+(defvar juick-user-name-regex "[\n ]\\(@[0-9A-Za-z\-]+\\)")
+(defvar juick-tag-regex "\\:[\n]\\(\\*[^ \n\t$]+\\)")
+(defvar juick-bold-regex "[\n ]\\(\\*.*\\*\\)[\n ]")
+(defvar juick-italic-regex "[\n ]\\(/.*/\\)[\n ]")
+(defvar juick-underline-regex "[\n ]\\(\\_.*\\_\\)[\n ]")
 
 (defun juick-add-overlay (begin end faces)
   (let ((overlay (make-overlay begin end)))
@@ -67,35 +91,55 @@
   (save-excursion
     (setq startmsg (re-search-backward "juick@juick.com>" nil t))
     (if startmsg
-	(while (re-search-forward
-	 	"\\(#[0-9]+\\(/[0-9]+\\)?\\)\\|[\t\n ]+\\(@[0-9A-Za-z\-]+\\)\\|\\:[\n]\\(\\*[^ \n\t$]+\\)" nil t)
-	  (if (match-string 1) ;; #NNNNNN
-	      (progn
-		(juick-add-overlay (match-beginning 1) (match-end 1)
-				   'juick-reply-id-face)
-		(make-button (match-beginning 1) (match-end 1)
-			     'action 'juick-insert-reply-id)))
-	  (if (match-string 3) ;; @user-name
-	      (progn
-		(juick-add-overlay (match-beginning 3) (match-end 3)
-				   'juick-user-name-face)
-		(make-button (match-beginning 3) (match-end 3)
-			     'action 'juick-insert-user-name)))
- 	  (if (match-string 4) ;; *tag
-	      (progn
-		(juick-add-overlay (match-beginning 4) (match-end 4)
-				   'juick-tag-face)
-		(make-button (match-beginning 4) (match-end 4)
-			     'action 'juick-find-tag)))))))
+        (while (re-search-forward
+                (concat juick-id-regex "\\|"
+                        juick-user-name-regex "\\|"
+                        juick-tag-regex "\\|"
+                        juick-bold-regex "\\|"
+                        juick-italic-regex "\\|"
+                        juick-underline-regex) nil t)
+          (cond ((match-string 1) ;; #NNNNNN
+                 (progn
+                   (juick-add-overlay (match-beginning 1) (match-end 1)
+                                      'juick-reply-id-face)
+                   (make-button (match-beginning 1) (match-end 1)
+                                'action 'juick-insert-reply-id)))
+                ((match-string 3) ;; @user-name
+                 (progn
+                   (juick-add-overlay (match-beginning 3) (match-end 3)
+                                      'juick-user-name-face)
+                   (make-button (match-beginning 3) (match-end 3)
+                                'action 'juick-insert-user-name)))
+                ((match-string 4) ;; *tag
+                 (progn
+                   (juick-add-overlay (match-beginning 4) (match-end 4)
+                                      'juick-tag-face)
+                   (make-button (match-beginning 4) (match-end 4)
+                                'action 'juick-find-tag)))
+                ((match-string 5) ;; *bold*
+                 (progn
+                   (juick-add-overlay (match-beginning 5) (match-end 5)
+                                      'juick-bold-face)
+                   (goto-char (- (point) 1)))) ;; next ' ' or '\n'
+                ((match-string 6) ;; /italic/
+                 (progn
+                   (juick-add-overlay (match-beginning 6) (match-end 6)
+                                      'juick-italic-face)
+                   (goto-char (- (point) 1)))) ;; next ' ' or '\n'
+                ((match-string 7) ;; _underline_
+                 (progn
+                   (juick-add-overlay (match-beginning 7) (match-end 7)
+                                      'juick-underline-face)
+                   (goto-char (- (point) 1))))))))) ;; next ' ' or '\n'
 
 (defun juick-insert-reply-id (button)
   "Inserting reply id"
   (save-excursion
     (goto-char (overlay-start button))
-    (re-search-forward "\\(#[0-9]+\\(/[0-9]+\\)?\\)" nil t)
-    (goto-char (point-max))
-    (setq id (match-string 0))
+    (re-search-forward juick-id-regex nil t)
+    (setq id (match-string 1))
     (set-text-properties 0 (length id) nil id)
+    (goto-char (point-max))
     (insert (concat id " ")))
   (goto-char (point-max))
   (recenter 10))
@@ -103,20 +147,20 @@
 (defun juick-insert-user-name (button)
   "Inserting reply id"
   (save-excursion
-    (goto-char (overlay-start button))
-    (re-search-forward "\\(@[0-9A-Za-z\-]+\\)" nil t)
-    (goto-char (point-max))
-    (setq id (match-string 0))
+    (goto-char (- (overlay-start button) 1)) ;; begining ' ' or '\n'
+    (re-search-forward juick-user-name-regex nil t)
+    (setq id (match-string 1))
     (set-text-properties 0 (length id) nil id)
+    (goto-char (point-max))
     (insert (concat id " ")))
   (goto-char (point-max))
   (recenter 10))
 
 (defun juick-find-tag (button)
-  "retrive 10 msg this tag"
+  "Retrive 10 msg this tag"
   (save-excursion
-    (goto-char (overlay-start button))
-    (re-search-forward "\\(\\*[^ \n\t$]+\\)" nil t)
+    (goto-char (- (overlay-start button) 2)) ;; begining ':\n'
+    (re-search-forward juick-tag-regex nil t)
     (setq tag (match-string 0))
     (set-text-properties 0 (length tag) nil tag)
     (delete-region jabber-point-insert (point-max)))
